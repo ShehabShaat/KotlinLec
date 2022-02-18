b8fdbe16080dad38924b9aef8af11e7ca840a98c
shehab shaat <shaatshehab@gmail.com>
2022-02-18 21:38:29 +0300

    update

---
+++ src/main/kotlin/MathOperations.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+//-----------Math Calculator-----------
+    print("Enter num 1 :")
+    var num1 : Int = readLine()!!.toInt()
+    print("Enter num 2 :")
+    var num2 : Int = readLine()!!.toInt()
+
+    var sum :  Int = num1+num2// => {*-+/}
+    print("sum is = $sum")
+}
+/*
+* put $ in any String when we put variable inside"string"
+* */
+
+
+
