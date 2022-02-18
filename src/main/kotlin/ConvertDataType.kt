95e04bb50a00b36686847e370cbfee92dc6283fb
shehab shaat <shaatshehab@gmail.com>
2022-02-18 21:15:51 +0300

    update

---
+++ src/main/kotlin/ConvertDataType.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+
+    //convert string to int value
+    var strInt : String = "123"
+    var intStr : Int = strInt.toInt()
+    println(intStr)
+    //convert string to double value
+    var strDouble : String = "123.5"
+    var doubleStr : Double = strDouble.toDouble()
+    println(doubleStr)
+    //convert int to double string
+    var intString: Int = 123
+    var strInteger : String = intString.toString()
+    println(strInteger)
+
+    //convert double to double int value
+    var doubleData:Double = 12.5
+    var intData : Int = doubleData.toInt()
+    print(intData)
+
+    //convert int to double double value
+    var intDData : Int = 12
+    var doubleIntData : Double = intDData.toDouble()
+    print(doubleIntData)
+
+
+
+}
