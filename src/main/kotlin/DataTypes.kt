3fa45075ae3e9869611b9458bf99cd87c4259f9e
shehab shaat <shaatshehab@gmail.com>
2022-02-18 12:21:04 +0300

    Kotlin Basics

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,13 @@
+// val name = "Shehab"
+fun main(args : Array<String>) {
+    var name = "shehab"
+    name = "shehab shaat"
+    print(name)
+}
+
+
+/*Important Notes
+*Always make the name of the variables indicate their name.
+* val ==> "Variable only to read"
+* var ==> "Adjustable variable"
+*  */
---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,5 @@
+
+fun main(args:Array<String>){
+    println("Welcome to Kotlin")
+    println(10)
+}
