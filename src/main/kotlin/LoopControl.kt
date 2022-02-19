4dc3bb18e2567cba90d39c1a5cb422e3e332713f
shehab shaat <shaatshehab@gmail.com>
2022-02-19 18:57:00 +0200

    update

---
+++ src/main/kotlin/LoopControl.kt
@@ -0,0 +1,39 @@
+fun main() {
+/**
+    loop@ for(item in 1..10)
+    {
+        for (item2 in 1..3) {//internal repetition
+            println("count $item ")
+            if (item == 5) {
+                break //In that case, it only "comes out "of internal repetition.
+            }
+        }
+    }
+
+ */
+
+
+    println("==========================")
+
+    loop@ for(item1 in 1..3)
+    {
+        for (item2 in 1..3) {
+            println("count $item1 ")
+            if (item2 == 3) {
+                break@loop//In that case, he "comes out "of all the repetition.
+            }
+        }
+
+    }
+
+    println("==========================")
+
+   /** for(item in 1..10)
+    {
+        if(item==5){
+            continue //In this case, he only skips number 5 and completes.
+        }
+        println("count $item ")
+
+    }*/
+}
